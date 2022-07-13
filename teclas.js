658fb678db96b4a76ea0eaa29d8797e6b979bc37
jessicadesangronis <jessicadesangronis@gmail.com>
2022-07-13 15:30:30 -0400

    se completó el código y ahora se puede dibujar

--- teclas.js
+++ teclas.js
@@ -1,42 +1,63 @@
 // crear un objeto con variables por dentro
 var teclas = {
-    UP=38,
+    UP: 38,
     DOWN: 40,
     LEFT: 37,
     RIGHT: 39
     };
-    //
     // todo lo anterior es una sola linea de codigo que se desplego para ver mejor. Buena Practica
     
-    // LA CONTANTES son un tipo de variable q se escriben con mayuscula. Buena Practica
+    // Las CONTANTES son un tipo de variable q se escriben con mayuscula. Buena Practica
     
-    document.addEventListener("keyup", dibujarTecado);
+    //console.log(teclas);
+    document.addEventListener("keyup", dibujarTeclado);
+    var cuadrito = document.getElementById("area_de_dibujo");
+    var papel = cuadrito.getContext("2d");
+    var x = 150;
+    var y = 150;
     
-function dibujarTeclado
-{
+    dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
     
-    if (evento.keyCode == teclas.UP)
+    function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, papel)
     {
-    console.log("tecla oprimida");
+      papel.beginPath();
+      papel.strokeStyle = color;
+      papel.lineWidth = 3;
+      papel.moveTo(xinicial, yinicial);
+      papel.lineTo(xfinal, yfinal);
+      papel.stroke();
+      papel.closePath();
     }
     
-    switch(evento.keyCode)
+    function dibujarTeclado(evento)
     {
-     case teclas.UP:
-     console.log("arriba");
-     
-     break;
-     
-     case teclas.DOWN:
-     console.log("abajo");
-     break;
-     
-     default:
-     console.log("OTRA tecla");
-     break;
-     
+    var colorcito = "#FAA";
+    var movimiento = 5;
+      switch(evento.keyCode)
+      {
+        case teclas.UP:
+           dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+           y = y - movimiento;
+           console.log("arriba");
+        break;
+        case teclas.DOWN:
+           dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+           y = y + movimiento;
+           console.log("abajo");
+        break;
+        case teclas.LEFT:
+    dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+        x = x - movimiento;
+            console.log("izquierda");
+        break;
+        case teclas.RIGHT:
+              dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+         x = x + movimiento;
+              console.log("derecha");
+        break;
+        default:
+            console.log("otra tecla");
+        break;
+      }
     }
     
-    
-    // el otro archivo
-}
